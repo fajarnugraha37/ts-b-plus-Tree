0fa73075187a2fa886370bd11d13583ee36ebb33
fajar <nugrahafajar37@gmail.com>
2025-11-30 05:15:51 +0700

    feat: Export snapshot functionality from the tree module

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { BPlusTree } from "./tree/bplusTree.ts";
 export type { InternalPage, LeafPage } from "./tree/pages.ts";
 export { PageType } from "./constants.ts";
 export type { KeyInput } from "./utils/codec.ts";
+export { exportSnapshot, importSnapshot } from "./tree/snapshot.ts";
